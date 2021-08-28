b1a7e7e82f2c0d6410ad3a15fa16811c72324d8b
Dima Boger <kotvberloge@gmail.com>
2021-08-28 18:23:56 +0300

    These commits will be just triggers for pages build

--- src/sidebar.js
+++ src/sidebar.js
@@ -6,14 +6,12 @@
 
 async function showPreview() {
   const selection = await miro.board.selection.get()
-  console.log('selection')
   clear()
-  const statByType = calcByType(selection)
-  getContainer().appendChild(createStatTable('by Type', 'Looks like the selection is empty.', statByType))
+  getContainer().appendChild(createPreview('by Type', 'Looks like the selection is empty.', selection))
 }
 
 function clear() {
-  const elements = getContainer().getElementsByClassName('stat-list__table')
+  const elements = getContainer().getElementsByClassName('preview-list__table')
   for (let i = 0; i < elements.length; i++) {
     elements.item(i).remove()
   }
@@ -23,30 +21,24 @@ function getContainer() {
   return document.getElementById('notes-container')
 }
 
-function createStatTable(title, emptyText, data) {
+function createPreview(title, emptyText, data) {
+  // filter only stickers
+  const stickers = data.filter(item => item.type.toLowerCase() === 'sticker')
+
   const statView = document.createElement('div')
-  statView.className = 'stat-list__table'
+  statView.className = 'preview-list__table'
 
   const titleView = document.createElement('div')
-  titleView.className = 'stat-list__title'
-  titleView.innerHTML = `<span>${title}</span>`
+  titleView.className = 'preview-list__title'
+  titleView.innerHTML = stickers.size === 0 ? '<span>Stickers to send</span>' : '<span>Select some stickers</span>'
   statView.appendChild(titleView)
 
-  if (data.size === 0) {
-    const emptyView = document.createElement('div')
-    emptyView.className = 'stat-list__empty'
-    emptyView.innerText = emptyText
-    statView.appendChild(emptyView)
-  } else {
-    data.forEach((value, key) => {
-      let itemView = document.createElement('div')
-      itemView.className = 'stat-list__item'
-      itemView.innerHTML =
-        `<span class="stat-list__item-name">${key.toLowerCase()}</span>` +
-        `<span class="stat-list__item-value">${value}</span>`
-      statView.appendChild(itemView)
-    })
-  }
+  stickers.forEach(sticker => {
+    let itemView = document.createElement('div')
+    itemView.className = 'preview-list__item'
+    itemView.innerHTML = `<span class="preview-list__item-value">${sticker}</span>`
+    statView.appendChild(itemView)
+  })
   return statView
 }
 
